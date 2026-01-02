afe4f6fcc94378aefe85d00cd37b94569c788ad3
ayush <ayush.ranjan921@gmail.com>
2026-01-02 11:07:10 +0530

    Progress with the page with transaction automation

--- src/test/java/pages/QAPlaygroundBank/QAPlaygroundTransaction.java
+++ src/test/java/pages/QAPlaygroundBank/QAPlaygroundTransaction.java
@@ -1,4 +1,72 @@
 package pages.QAPlaygroundBank;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+//To make transaction across available account
 public class QAPlaygroundTransaction {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    WebElement webElement;
+    Select select = new Select(webElement);
+
+    public QAPlaygroundTransaction(WebDriver webDriver, WebDriverWait webDriverWait) {
+        this.driver = webDriver;
+        this.wait = webDriverWait;
+
+    }
+
+    private By transactionCTA = By.id("new-transaction-link");
+    private By modalTitle = By.id("modal-title");
+    private By transactionTypeDropdown = By.xpath("(//select[contains(@style,'position: absolute')])[last()-1]");
+    private By fromAccountDropdown = By.xpath("(//select[contains(@style,'position: absolute')])[last()]");
+    private By amountInputField = By.id("transaction-amount");
+    private By descriptionInputField = By.id("transaction-description");
+    private By sendNotificationCheckBox = By.id("send-notification");
+    private By cancelCTA = By.id("cancel-transaction-btn");
+    private By submitTransaction = By.id("submit-transaction-btn");
+
+    public void transactionCTAClick() {
+        wait.until(ExpectedConditions.elementToBeClickable(transactionCTA));
+        driver.findElement(transactionCTA).click();
+
+        //Adding transaction dialog box heading grab to verify if the section was opened successfully
+        wait.until(ExpectedConditions.visibilityOfElementLocated(modalTitle));
+        System.out.println(driver.findElement(modalTitle).getText());
+    }
+
+
+    /*TODO: First add manual dropdown option selection from the dropdown of the transaction type, and then it can be improved for selecting random option from the dropdown for transaction type and from account*/
+
+    public void selectTransactionType() {
+        wait.until(ExpectedConditions.elementToBeClickable(transactionTypeDropdown));
+        webElement = driver.findElement(transactionTypeDropdown);
+        select = new Select(webElement);
+
+        List<WebElement> transactionDropdownOptions = select.getOptions();
+//        for (WebElement option : transactionDropdownOptions) {
+//            System.out.println(option.getText());
+//        }
+        select.selectByValue(transactionDropdownOptions.getFirst().getDomAttribute("value"));
+
+    }
+
+    public void selectFromAccount(){
+        wait.until(ExpectedConditions.elementToBeClickable(fromAccountDropdown));
+        webElement = driver.findElement(fromAccountDropdown);
+        select = new Select(webElement);
+
+        List<WebElement> fromAccountOptions = select.getOptions();
+
+        select.selectByValue(fromAccountOptions.getLast().getDomAttribute("value"));
+    }
 }
--- src/test/java/tests/QAPlaygroundBank/DashboardTest.java
+++ src/test/java/tests/QAPlaygroundBank/DashboardTest.java
@@ -3,13 +3,23 @@ package tests.QAPlaygroundBank;
 import base.BaseTestClass;
 import org.testng.annotations.Test;
 import pages.QAPlaygroundBank.QAPlaygroundDashboardPage;
+import pages.QAPlaygroundBank.QAPlaygroundTransaction;
 
 public class DashboardTest extends BaseTestClass {
     QAPlaygroundDashboardPage dashboardPage;
+    QAPlaygroundTransaction transactionAction;
 
-    @Test
+    @Test(priority = 1)
     public void balanceCheck(){
         dashboardPage = new QAPlaygroundDashboardPage(webDriver,wait);
         System.out.println(dashboardPage.getBalance());
+    }
+
+    @Test(priority = 2)
+    public void transactionCheck(){
+        transactionAction = new QAPlaygroundTransaction(webDriver,wait);
+        transactionAction.transactionCTAClick();
+        transactionAction.selectTransactionType();
+        transactionAction.selectFromAccount();
     }
 }
